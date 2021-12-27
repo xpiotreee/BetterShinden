dcecdc07bf8537bdb2a3da32131048c80a0011b5
xpiotreee <piotrowski.00@wp.pl>
2021-12-27 18:41:50 +0100

    tags tooltips

--- src/components/Tag.svelte
+++ src/components/Tag.svelte
@@ -1,7 +1,13 @@
 <script lang="ts">
-    import type * as Api from '../Interfaces';
-
+    import type * as Api from "../Interfaces";
+    import tippy from "sveltejs-tippy";
     export let tag: Api.Tag;
+
+    const props = {
+        content: tag.description
+    }
 </script>
 
-<a class="p-1 m-0.75 rounded-md border-pink-400 border-1" href=" "> {tag.name} </a>
+<a use:tippy={props} class="p-1 m-0.75 rounded-md border-pink-400 border-1" href=" ">
+    {tag.name}
+</a>
